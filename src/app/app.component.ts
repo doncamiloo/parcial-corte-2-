f547f8b9c46416d9968d4d49a967e3fad41d05ae
Camiloqqq <114710196+Camiloqqq@users.noreply.github.com>
2023-10-14 02:27:04 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'parcialNASA';
+  title = 'PARCIAL NASA';
+
+
+  altura: number = 0;
+  tiempo: number = 0;
+  velocidad: number = 0;
+  gravedad: number = 0;
+  readonlyInput: boolean = true;
+
+  H(){
+    const seconds = this.tiempo * 60;
+this.altura = (this.gravedad * (seconds ** 2)) / 2;
+this.altura = Math.round(this.altura * 100) / 100;
+  }
+
+  V_f() {
+    const seconds = this.tiempo * 60;
+    this.velocidad = this.gravedad * seconds;
+    this.velocidad = Math.round(this.velocidad * 100) / 100;
+  }
 }
